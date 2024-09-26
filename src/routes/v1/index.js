5341662a5211954be0c7da1ad5bbcb1e14a403a3
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-09-26 08:18:57 +0700

    add basic routes

---
+++ src/routes/v1/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+const Router = express.Router()
+// check api v1
+Router.get('/status', (req, res) => {
+  res
+    .status(StatusCodes.OK)
+    .json({ message: 'API v1 already to use.', status: StatusCodes.OK })
+})
+//board APIs
+Router.use('/boards', boardRoutes)
+
+export const APIs_v1 = Router
--- src/server.js
+++ src/server.js
@@ -3,13 +3,11 @@ import express from 'express'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import exitHook from 'async-exit-hook'
 import { env } from '~/config/environment'
+import { APIs_v1 } from '~/routes/v1'
 const START_SERVER = () => {
   const app = express()
 
-  app.get('/', async (req, res) => {
-    console.log(env)
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_v1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
